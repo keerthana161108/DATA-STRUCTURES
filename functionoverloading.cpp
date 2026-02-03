e70a85deaea72eff65a54374c8e9ee17b7094dd2
keerthana161108 <keerthana.mncl@gmail.com>
2026-02-03 15:28:38 +0530

    Add files via upload

---
+++ functionoverloading.cpp
@@ -0,0 +1,21 @@
+//function over loading
+#include<iostream>
+using namespace std;
+class math{
+    public:
+    int add(int a,int b){
+        return a+b;
+    }
+    float add(float a,float b){
+    return a+b;
+    }
+    int add(int a,int b,int c){
+        return a+b+c;
+    }
+};
+int main (){
+    math m;
+    cout<<"add two integers:"<<m.add(10,20)<<endl;
+    cout<<"add two floats:"<<m.add(2.5f,4.5f)<<endl;
+    return 0;
+}
